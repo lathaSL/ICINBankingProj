53be2559cfaeb76c7b4ea1e1bbd9ef971d35cfc2
sree <sree.sanchana@gmail.com>
2022-05-27 20:37:22 +0530

    code checkin

---
+++ accountdtls/src/test/java/com/icin/accountdtls/AccountdtlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icin.accountdtls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountdtlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
